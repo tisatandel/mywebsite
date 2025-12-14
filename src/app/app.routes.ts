c57aa662d44d238796eb6eb17145c87ba0be1686
Tisa <tisatandel@gmail.com>
2025-12-14 22:54:28 +0530

    category created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
 
 export const routes: Routes = [
-    { path:'', component:Home},
+    { path:'home', component:Home},
+    { path:'about', component:About},
+    { path:'contact', component:Contact},
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet} from '@angular/router';
+import { RouterLink, RouterOutlet} from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from "./footer/footer";
 
