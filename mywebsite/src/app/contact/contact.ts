db5c72d949126a841878245c0ab00bd862597cd2
Tisa <tisatandel@gmail.com>
2025-12-18 16:40:53 +0530

    hooks

--- mywebsite/src/app/contact/contact.ts
+++ mywebsite/src/app/contact/contact.ts
@@ -1,26 +1,88 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnDestroy,
+  OnInit
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-contact',
+  standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './contact.html',
   styleUrls: ['./contact.css'],
 })
-export class Contact {
+export class Contact implements
+  OnInit,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
 
   nameInput = '';
   emailInput = '';
   subjectInput = '';
   messageInput = '';
 
+  buttonColor: string = 'green';
+  private colorInterval: any;
+
   messages: any[] = [];
   editIndex: number | null = null;
 
+  constructor() {
+    console.log('Constructor called');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+    this.startColorChange(); // 🔑 start color change automatically
+  }
+
+  ngDoCheck(): void 
+  { 
+    console.log('ngDoCheck called');
+  }
+  ngAfterContentInit(): void 
+  { 
+    console.log('ngAfterContentInit called'); 
+  }
+  ngAfterContentChecked(): void 
+  { 
+    console.log('ngAfterContentChecked called'); 
+  }
+  ngAfterViewInit(): void 
+  { 
+    console.log('ngAfterViewInit called'); 
+  }
+  ngAfterViewChecked(): void 
+  { 
+    console.log('ngAfterViewChecked called'); 
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
+    clearInterval(this.colorInterval);
+  }
+
+
+  startColorChange(): void {
+    this.colorInterval = setInterval(() => {
+      this.buttonColor = this.buttonColor === 'green' ? 'orange' : 'green';
+    }, 3000);
+  }
+
+  // Add / Update message
   add() {
     if (this.editIndex === null) {
-      // ADD NEW
       this.messages.push({
         name: this.nameInput,
         email: this.emailInput,
@@ -28,7 +90,6 @@ export class Contact {
         message: this.messageInput
       });
     } else {
-      // UPDATE EXISTING
       this.messages[this.editIndex] = {
         name: this.nameInput,
         email: this.emailInput,
@@ -38,7 +99,7 @@ export class Contact {
       this.editIndex = null;
     }
 
-    // Clear inputs
+    // Clear form
     this.nameInput = '';
     this.emailInput = '';
     this.subjectInput = '';
@@ -50,7 +111,6 @@ export class Contact {
     this.emailInput = this.messages[index].email;
     this.subjectInput = this.messages[index].subject;
     this.messageInput = this.messages[index].message;
-
     this.editIndex = index;
   }
 
